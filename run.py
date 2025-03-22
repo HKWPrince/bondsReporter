2329d879c38a3b9ad41b613bf50e6627338d0c83
HKWPrince <prince880211@gmail.com>
2025-03-22 20:32:35 +0800

    2025022_7

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import atexit
+import os
 from app import create_app
 
 app = create_app()
@@ -5,3 +7,7 @@ app = create_app()
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
+def shutdown_docker():
+    os.system("docker-compose down")
+
+atexit.register(shutdown_docker)
