8eee676ec3c856714c2ecb07a7e58ac342eec72d
HKWPrince <prince880211@gmail.com>
2025-03-22 19:30:46 +0800

    2025022_2

--- BondsReporter.bat
+++ BondsReporter.bat
@@ -1,7 +1,8 @@
 @echo off
+echo 1/5 Updating project from GitHub...
 git pull origin main
 
-echo Checking if Docker is running...
+echo 2/5 Checking if Docker is running...
 
 :: 嘗試取得 Docker 服務狀態
 tasklist | findstr "Docker Desktop.exe" >nul
@@ -21,7 +22,7 @@ if %ERRORLEVEL% NEQ 0 (
     )
 )
 
-echo Docker is running. Continuing execution...
+echo 3/5 Docker is running. Continuing execution...
 
 :: 進入 Docker 目錄
 cd docker
@@ -39,19 +40,19 @@ timeout /t 5 /nobreak >nul
 :: 檢查 Docker 容器是否運行
 docker ps | findstr "mssql_container" >nul
 if %ERRORLEVEL% NEQ 0 (
-    echo Docker services failed to start. Please check your Docker setup.
+    echo 4/5 Docker services failed to start. Please check your Docker setup.
     pause
     exit /b 1
 )
 
 echo BondsReporter is running...
 echo Available services:
-docker ps --format "table {{.Names}}\t{{.Ports}}"
+
 
 :: 確保 Python 存在
 where python >nul 2>nul
 if %ERRORLEVEL% NEQ 0 (
-    echo Python is not installed. Please install Python and try again.
+    echo 5/5 Python is not installed. Please install Python and try again.
     pause
     exit /b 1
 )
